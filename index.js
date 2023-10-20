f1875584575d1ba6be7f077aceb8c913ef6edac5
mjs001 <50973969+mjs001@users.noreply.github.com>
2023-10-20 18:22:52 -0500

    updated wip still

--- index.js
+++ index.js
@@ -12,10 +12,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var currentPage = 1;
 var keyword = "";
 var pageSize = 13;
-var sortBy = "relevancy";
 var apiKey = process.env.API_TOKEN;
 var category = "general";
-
+var newCategory = "general";
 var date = new Date();
 var year = date.getFullYear();
 var day = ("0" + date.getDate()).slice(-2);
@@ -23,18 +22,22 @@ var month = ("0" + (date.getMonth() + 1)).slice(-2);
 var todaysDate = `${month}-${day}-${year}`;
 
 app.get("/", async (req, res) => {
+    // if category same as old just get localstorage otherwise run new request
+
     var parsedDate = localStorage.getItem("date");
     var parsedStoredData = JSON.parse(localStorage.getItem("storedData"));
-    if (parsedStoredData !== null && parsedDate === todaysDate) {
+    console.log(newCategory, category);
+    if (parsedStoredData !== null && parsedDate === todaysDate && newCategory === category) {
         res.render("index.ejs", {
             data: parsedStoredData
         });
     } else {
         try {
+            category = newCategory;
             var auth = {
                 headers: { Authorization: `Bearer ${apiKey}` },
             };
-            const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&pageSize=${pageSize}&sortBy=${sortBy}&currentPage=${currentPage}`, auth);
+            const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&pageSize=${pageSize}&currentPage=${currentPage}`, auth);
             var data = response.data;
             var filteredData = data.articles.filter((article) => {
                 return article.url !== 'https://removed.com' && article.url !== null && article.title !== null && article.description !== null && article.title !== '[Removed]';
@@ -53,29 +56,20 @@ app.get("/", async (req, res) => {
            while (filteredData.length > 10) {
                 filteredData = filteredData.slice(0, -1);
             }
+
             localStorage.setItem("date", todaysDate);
             localStorage.setItem("storedData", JSON.stringify(filteredData));
             res.render("index.ejs", { data: filteredData });
         } catch (error) {
-            console.log(error);
             res.render("index.ejs", { error: error });
         }
     }
 })
 
-app.get("/search", (req, res) => {
-    try {
-        var auth = {
-            headers: { Authorization: `Bearer ${apiKey}` },
-        };
-        const response = axios.get(`https://newsapi.org/v2/top-headlines?country=us&q=${keyword}&pageSize=${pageSize}&page=${page}`, auth);
-        var data = response.data;
-        res.render("index.ejs", { data: data });
-    } catch (error) {
-        console.log(response.data.error);
-        res.render("index.ejs", { error: response.data.error });
-    }
-
+app.post("/category", (req, res) => {
+    console.log(req.body.category);
+    newCategory = req.body.category;
+    res.redirect("/");
 })
 
 app.listen(port, () => {
